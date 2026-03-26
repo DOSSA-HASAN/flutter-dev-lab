d23241c68146c9695dcaa73679c06308678366d3
Hasan <hassandossa5@gmail.com>
2026-03-26 09:02:20 +0300

    feat: added other things

--- lib/widgets/learn_slivers.dart
+++ lib/widgets/learn_slivers.dart
@@ -9,17 +9,32 @@ class LearnSlivers extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
-            expandedHeight: 100,
             pinned: true,
+            expandedHeight: 200,
+            collapsedHeight: 100,
             flexibleSpace: FlexibleSpaceBar(
-              title: Text("Sliver Lab"),
-              background: Container(color: Colors.purple),
+              title: Text("Sliver App Bar - Lab"),
             ),
           ),
-          SliverList(
+          SliverToBoxAdapter(
+            child: Container(
+              height: 100,
+              color: Colors.amber,
+              child: const Center(
+                child: Text("I am a normal box in a sliver world"),
+              ),
+            ),
+          ),
+          SliverGrid(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              mainAxisSpacing: 5,
+              crossAxisSpacing: 5,
+            ),
             delegate: SliverChildBuilderDelegate(
-              (context, index) => ListTile(title: Text("Sliver Item $index")),
-              childCount: 50
+              (content, index) =>
+                  Container(color: Colors.purple[100 * (index % 9)]),
+              childCount: 50,
             ),
           ),
         ],
