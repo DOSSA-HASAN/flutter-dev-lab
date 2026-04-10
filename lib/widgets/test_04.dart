d8f41e667001d99f784fddf12786b69a404963ef
Hasan <hassandossa5@gmail.com>
2026-04-10 09:27:40 +0300

    feat: added theme context stylings

--- lib/widgets/test_04.dart
+++ lib/widgets/test_04.dart
@@ -6,68 +6,68 @@ class Test04 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            pinned: true,
-            expandedHeight: 250,
-            backgroundColor: Colors.deepPurpleAccent,
-            flexibleSpace: FlexibleSpaceBar(title: Text("Explore Apps")),
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                decoration: InputDecoration(border: OutlineInputBorder()),
-              ),
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              pinned: true,
+              expandedHeight: 250,
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              flexibleSpace: FlexibleSpaceBar(title: Text("Explore Apps")),
             ),
-          ),
-          SliverToBoxAdapter(
-            child: SizedBox(
-              height: 180,
-              child: ListView.builder(
-                itemCount: 5,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) => Container(
-                  width: (MediaQuery.of(context).size.width / 5),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.purple,
-                      style: BorderStyle.solid,
-                    ),
-                    color: Colors.deepPurpleAccent[(index % 5) * 100],
-                  ),
-                  child: Center(child: Text('$index')),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: TextField(
+                  decoration: InputDecoration(border: OutlineInputBorder()),
                 ),
               ),
             ),
-          ),
-          SliverGrid(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              crossAxisSpacing: 5,
-              mainAxisSpacing: 5
-            ),
-            delegate: SliverChildBuilderDelegate(
-              (context, index) => Container(
-                decoration: BoxDecoration(color: Colors.green),
-                height: 50,
-                width: 50,
+            SliverToBoxAdapter(
+              child: SizedBox(
+                height: 180,
+                child: ListView.builder(
+                  itemCount: 5,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) => Container(
+                    width: (MediaQuery.of(context).size.width / 5),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Theme.of(context).colorScheme.secondary,
+                        style: BorderStyle.solid,
+                      ),
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    child: Center(child: Text('$index')),
+                  ),
+                ),
               ),
-              childCount: 6,
             ),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) => ListTile(
-                tileColor: Colors.red,
-                title: Text("Recomended Apps"),
+            SliverGrid(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 5,
+                mainAxisSpacing: 5,
               ),
-              childCount: 20,
+              delegate: SliverChildBuilderDelegate(
+                (context, index) => Container(
+                  decoration: BoxDecoration(color: Colors.green),
+                  height: 50,
+                  width: 50,
+                ),
+                childCount: 6,
+              ),
             ),
-          ),
-        ],
-      ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) => ListTile(
+                  tileColor: Theme.of(context).colorScheme.secondary,
+                  title: Text("Recomended Apps"),
+                ),
+                childCount: 20,
+              ),
+            ),
+          ],
+        ),
     );
   }
 }
