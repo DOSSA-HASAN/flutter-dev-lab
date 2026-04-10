a89844d99a46f3a9f18429a5c9dc55fbdaa99ebe
Hasan <hassandossa5@gmail.com>
2026-04-10 08:35:55 +0300

    feat: added GridView practice

--- lib/widgets/re_learn_lists_and_grids.dart
+++ lib/widgets/re_learn_lists_and_grids.dart
@@ -6,13 +6,23 @@ class ReLearnListsAndGrids extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
+      body: GridView.builder(
         itemCount: 20,
-        itemBuilder: (context, index) => ListTile(
-          leading: const Icon(Icons.list),
-          title: Text("Normal list $index"),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1,
         ),
+        itemBuilder: (context, index) =>
+            Card(child: Center(child: Text("Grid $index"))),
       ),
+
+      // ListView.builder(
+      //   itemCount: 20,
+      //   itemBuilder: (context, index) => ListTile(
+      //     leading: const Icon(Icons.list),
+      //     title: Text("Normal list $index"),
+      //   ),
+      // ),
     );
   }
 }
