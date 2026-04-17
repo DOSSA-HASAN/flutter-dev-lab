f8436dcc0f3aaf2fab7a02ebe975aa344f169ac6
Hasan <hassandossa5@gmail.com>
2026-04-17 11:20:59 +0300

    feat: added AppModel class to encapsulate app metadata

---
+++ lib/models/app_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppModel {
+  final String name;
+  final String category;
+  final Color themeColor;
+
+  AppModel({
+    required this.name,
+    required this.category,
+    required this.themeColor,
+  });
+}
